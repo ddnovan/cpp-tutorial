527e707bdc77dbb205b7bd783cb427c8ee1e59f7
dnovanull <alu0101478999@ull.edu.es>
2025-07-12 18:51:05 +0100

    chore(global): Update main with chapters code

--- tutorial/src/main.cc
+++ tutorial/src/main.cc
@@ -2,6 +2,13 @@
 
 int main()
 {
-	std::cout << "Hello, world!";
+	// Chapter 1.1
+	std::cout << "Hello, world!" << std::endl;
+	// Chapter 1.2: (Implícito con comentarios)
+	// Chapter 1.3
+	std::cout << 5 << std::endl;
+	std::cout << -6.7 << std::endl;
+	std::cout << 'H' << std::endl;
+	std::cout << "Texto" << std::endl;
 	return 0;
 }
